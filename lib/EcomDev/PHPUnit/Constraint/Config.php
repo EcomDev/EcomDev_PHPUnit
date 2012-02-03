37228366bf4478d0e05455d28ed2eca54e5352d3
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2012-02-03 10:10:38 +0200

    ! Hotfix PHPUnit 3.6 with not assertments hungup

--- lib/EcomDev/PHPUnit/Constraint/Config.php
+++ lib/EcomDev/PHPUnit/Constraint/Config.php
@@ -108,4 +108,21 @@ class EcomDev_PHPUnit_Constraint_Config extends PHPUnit_Framework_Constraint
     {
         return $this->constraint->toString();
     }
+
+    /**
+     * Returns the description of the failure
+     *
+     * The beginning of failure messages is "Failed asserting that" in most
+     * cases. This method should return the second part of that sentence.
+     *
+     * To provide additional failure information additionalFailureDescription
+     * can be used.
+     *
+     * @param  mixed $other Evaluated value or object.
+     * @return string
+     */
+    protected function failureDescription($other)
+    {
+        return $this->constraint->failureDescription($other);
+    }
 }
