0591c3d31b7288db5980f7f2c95f7049205c9183
Pablo Bender <pablo@bender.eti.br>
2013-03-07 10:48:42 -0400

    fix EcomDev_PHPUnit_Test_Case::setUp for error 'Cannot run controller test, because the host is not set for base url.' for tests based on EcomDev_PHPUnit_Test_Case that uses singleton customer/session. ex: helper test for Product::getFinalPrice

--- app/code/community/EcomDev/PHPUnit/Test/Case.php
+++ app/code/community/EcomDev/PHPUnit/Test/Case.php
@@ -705,4 +705,17 @@ abstract class EcomDev_PHPUnit_Test_Case extends PHPUnit_Framework_TestCase
         TestUtil::setCurrentStore($store);
         return $this;
     }
+
+    /**
+     * Set up controller params
+     * (non-PHPdoc)
+     * @see PHPUnit_Framework_TestCase::setUp()
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $_SESSION = array();
+        $_baseUrl = Mage::getStoreConfig('web/unsecure/base_url');
+        $this->app()->getRequest()->setBaseUrl($_baseUrl);
+    }
 }
