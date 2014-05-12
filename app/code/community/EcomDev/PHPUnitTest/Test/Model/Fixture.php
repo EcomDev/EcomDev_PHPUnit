8eaaefd062d9d416828035020672a3b9d6828840
Kristof Ringleff <kristof@fooman.co.nz>
2014-05-12 21:31:22 +1200

    test case for config values from multiple files - expected behaviour would be that the most specific one prevails

--- app/code/community/EcomDev/PHPUnitTest/Test/Model/Fixture.php
+++ app/code/community/EcomDev/PHPUnitTest/Test/Model/Fixture.php
@@ -17,6 +17,18 @@ class EcomDev_PHPUnitTest_Test_Model_Fixture extends EcomDev_PHPUnit_Test_Case
         $this->getFixture()->apply();
     }
 
+    public function testLoadClassBeforeMethodFixtures()
+    {
+        require_once($this->_getVfsUrl('app/code/community/EcomDev/PHPUnit/Test/Model/ExampleClass.php'));
+
+        $testCase = new EcomDev_PHPUnitTest_Test_Model_ExampleClass();
+        $testCase->setName('testLoadFixtureOrder');
+        $this->getFixture()->loadForClass(get_class($testCase));
+        $this->getFixture()->loadByTestCase($testCase);
+        $this->getFixture()->apply();
+        $this->assertEquals('methodFixtureValue', Mage::getStoreConfig('sample/path'));
+    }
+
     protected function _getVfsUrl($path)
     {
         return $this->getFixture()->getVfs()->url($path);
